a32653597be96ae8ab04dae9b054e5ef2da20210
Maxim <belyaevm858@gmail.com>
2023-12-21 02:41:53 +0500

    comm1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,38 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(initialCards, deleteCallback) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.content.cloneNode(true).querySelector('.places__item');
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = initialCards.link;
+  cardImage.alt = initialCards.name;
+  cardTitle.textContent = initialCards.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  const placesList = document.querySelector('.places__list');
+  placesList.removeChild(cardElement);
+}
+
+function renderCards() {
+  const placesList = document.querySelector('.places__list');
+  
+  initialCards.forEach(function(initialCards) {
+    const cardElement = createCard(initialCards, deleteCard);
+    placesList.appendChild(cardElement);
+  });
+}
+
+renderCards();
